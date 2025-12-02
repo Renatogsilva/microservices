fa173e5248f6918fe6f46ac8893e6c7ac16b3a48
Renato Gomes <silvarenato180@gmail.com>
2025-12-02 09:54:13 -0300

    Commit de criação estrutural dos microservices

---
+++ Microservices/Delivery-Tracking/src/test/java/com/br/renatorgs/redelivery/delivery/tracking/DeliveryTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.renatorgs.redelivery.delivery.tracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
