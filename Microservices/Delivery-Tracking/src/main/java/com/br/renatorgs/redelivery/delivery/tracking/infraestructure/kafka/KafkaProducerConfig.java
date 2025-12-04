e9a04309a994da9cbb80ab17495c508e7e2aebab
Renato Gomes <silvarenato180@gmail.com>
2025-12-04 09:09:26 -0300

    Commit de implementação dos microservices

--- Microservices/Delivery-Tracking/src/main/java/com/br/renatorgs/redelivery/delivery/tracking/infraestructure/kafka/KafkaProducerConfig.java
+++ Microservices/Delivery-Tracking/src/main/java/com/br/renatorgs/redelivery/delivery/tracking/infraestructure/kafka/KafkaProducerConfig.java
@@ -1,13 +1,11 @@
 package com.br.renatorgs.redelivery.delivery.tracking.infraestructure.kafka;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.kafka.support.serializer.JsonSerde;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -17,15 +15,14 @@ public class KafkaProducerConfig {
 
     @Bean
     public ProducerFactory<String, Object> producerFactory() {
-
         Map<String, Object> props = new HashMap<>();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
 
-        return new DefaultKafkaProducerFactory<>(
-                props,
-                new StringSerializer(),
-                new JsonSerde<>().serializer()
-        );
+        // Serializers modernos
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, org.apache.kafka.common.serialization.StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, org.springframework.kafka.support.serializer.JsonSerializer.class);
+
+        return new DefaultKafkaProducerFactory<>(props);
     }
 
     @Bean
